78168b072c094eca2110110e3449a8bbcbb27472
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 18:43:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if uploaded_file is not None:
         st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
     uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
     
-    if uploaded_file is not None:
+if uploaded_file is not None:
     image = Image.open(uploaded_file)
     
     col1, col2 = st.columns( [0.5, 0.5])
