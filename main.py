5a3ac6516bcda232d9f1d46634fa518123505576
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 20:02:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ with col1:               # To display the header text using css style
     st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
     
 #Add a header and expander in side bar
-st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
+st.sidebar.markdown('<p class="font">Aplikasi Konversi Foto</p>', unsafe_allow_html=True)
 with st.sidebar.expander("About the App"):
      st.write("""
-        Use this simple app to convert your favorite photo to a pencil sketch, a grayscale image or an image with blurring effect.  \n  \nThis app was created by Sharone Li as a side project to learn Streamlit and computer vision. Hope you enjoy!
+        Konversi fotomu dengan beberapa filter di bawah  \n  \nCreated by Ricky - project Pengolahan Citra Digital
      """)
 uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
 
