b025b618b525b0a029e81c00d535ae68db8c8bcd
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 18:40:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
-from rembg import remove
-from PIL import Image
-from matplotlib import pyplot as plt
 import streamlit as st
+import numpy as np
+import cv2
+from  PIL import Image, ImageEnhance
 
-def tampil(img):
-    plt.imshow(img)
-    plt.show()
-    st.image(img)
-  
+image = Image.open(r'...\Insights_Bees_logo.png') #Brand logo image (optional)
+
+#Create two columns with different width
+col1, col2 = st.columns( [0.8, 0.2])
+with col1:               # To display the header text using css style
+    st.markdown(""" <style> .font {
+    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
     
-if __name__=="__main__":
-   
-    st.sidebar.subheader('Upload Image')
-    img = st.sidebar.file_uploader(label='Pilih Gambar', type=('jpg'))
-      
-    if img is not None:
-        st.sidebar.write('File Uploaded')
-        try:
-           tampil(img)
-            
-        except Exception as e:
-            print(e)
-           
+with col2:               # To display brand logo
+    st.image(image,  width=150)
  
+#Add a header and expander in side bar
+st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
+with st.sidebar.expander("About the App"):
+     st.write("""
+        Use this simple app to convert your favorite photo to a pencil sketch, a grayscale image or an image with blurring effect.  \n  \nThis app was created by Sharone Li as a side project to learn Streamlit and computer vision. Hope you enjoy!
+     """)
+     uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+    if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    
+    col1, col2 = st.columns( [0.5, 0.5])
+    with col1:
+        st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+        st.image(image,width=300)  
+
+    with col2:
+        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+    
+    if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    
+    col1, col2 = st.columns( [0.5, 0.5])
+    with col1:
+        st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+        st.image(image,width=300)  
+
+    with col2:
+        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+        filter = st.sidebar.radio('Covert your photo to:', ['Original','Black and White', 'Pencil Sketch', 'Blur Effect']) #Add the filter in the sidebar
+
    
    
 
