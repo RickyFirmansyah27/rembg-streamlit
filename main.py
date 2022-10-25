035db5d4eaa1ade41158fbb4d50208cf907b9577
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 19:23:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,5 +59,6 @@ if uploaded_file is not None:
                 st.image(blur_image, channels='BGR', width=300) 
         else: 
                 st.image(image, width=300)
-        btn = download_button(label="Download Image",data=byte_im,file_name="imagename.png",mime="image/jpeg",)
+        
+        download_button(label="Download Image",data=byte_im,file_name="imagename.png",mime="image/jpeg",)
 
