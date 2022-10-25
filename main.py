4eebe3e3246145a2a40d6c6b0d409ae1bef1092a
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 17:34:51 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from rembg import remove
+from PIL import image
+from matplotlib import pyplot as plt
+import streamlit
+
+def main():
+    input = Image.open(file)
+    output_path = 'output.png'
+    output = remove(input)
+    plt.imshow(output_path)
+    plt.show()
+  
+    
+if __name__=="__main__":
+   
+    st.sidebar.subheader('Upload Image')
+    file = st.sidebar.file_uploader(label='Pilih Gambar', type=('jpg'))
+      
+    if file is not None:
+        st.sidebar.write('File Uploaded')
+        try:
+           main()
+            
+        except Exception as e:
+            print(e)
+           
+ 
+   
+   
+
+
+
+
+
+
+
+                                                                                                   
