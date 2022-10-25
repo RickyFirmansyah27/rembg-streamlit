b737437cc3e7458393b09efed6d425b14eff1ffc
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 18:57:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,7 @@ with st.sidebar.expander("About the App"):
      """)
 uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
     
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    col1, col2 = st.columns( [0.5, 0.5])
-    with col1:
-        st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
-        st.image(image,width=300)  
-
-    with col2:
-        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
-    uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
-    
+#Add 'before' and 'after' columns
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     
@@ -42,40 +32,6 @@ if uploaded_file is not None:
 
     with col2:
         st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
-        filter = st.sidebar.radio('Covert your photo to:', ['Original','Black and White', 'Pencil Sketch', 'Blur Effect']) #Add the filter in the sidebar
- 
-with col2:
-        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
-        filter = st.sidebar.radio('Covert your photo to:', ['Original','Gray Image','Black and White', 'Pencil Sketch', 'Blur Effect'])
-        if filter == 'Gray Image':
-                converted_img = np.array(image.convert('RGB'))
-                gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-                st.image(gray_scale, width=300)
-        elif filter == 'Black and White':
-                converted_img = np.array(image.convert('RGB'))
-                gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-                slider = st.sidebar.slider('Adjust the intensity', 1, 255, 127, step=1)
-                (thresh, blackAndWhiteImage) = cv2.threshold(gray_scale, slider, 255, cv2.THRESH_BINARY)
-                st.image(blackAndWhiteImage, width=300)
-        elif filter == 'Pencil Sketch':
-                converted_img = np.array(image.convert('RGB')) 
-                gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
-                inv_gray = 255 - gray_scale
-                slider = st.sidebar.slider('Adjust the intensity', 25, 255, 125, step=2)
-                blur_image = cv2.GaussianBlur(inv_gray, (slider,slider), 0, 0)
-                sketch = cv2.divide(gray_scale, 255 - blur_image, scale=256)
-                st.image(sketch, width=300) 
-        elif filter == 'Blur Effect':
-                converted_img = np.array(image.convert('RGB'))
-                slider = st.sidebar.slider('Adjust the intensity', 5, 81, 33, step=2)
-                converted_img = cv2.cvtColor(converted_img, cv2.COLOR_RGB2BGR)
-                blur_image = cv2.GaussianBlur(converted_img, (slider,slider), 0, 0)
-                st.image(blur_image, channels='BGR', width=300) 
-        else: 
-                st.image(image, width=300)
-
-
-   
    
 
 
