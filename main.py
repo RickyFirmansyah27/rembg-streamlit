d889499c6e2570adbbeb8c16a6ae534ad621607f
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-25 18:58:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,18 @@ if uploaded_file is not None:
     with col2:
         st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
    
+uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    
+    col1, col2 = st.columns( [0.5, 0.5])
+    with col1:
+        st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
+        st.image(image,width=300)  
 
+    with col2:
+        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+        filter = st.sidebar.radio('Covert your photo to:', ['Original','Black and White', 'Pencil Sketch', 'Blur Effect']) #Add the filter in the sidebar
 
 
 
