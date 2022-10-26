3bcd5a8092ea89adcae3678067a99ebbf0104ac1
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-10-26 18:06:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -88,8 +88,8 @@ if uploaded_file is not None:
                 response:requests.post(
                     'https://api.remove.bg/v1.0/removebg',
                     files={'image_file': open(img, 'rb')},
-                    data={'size':'auto','bg_color':color},
-                    headers={'X-Api-Key': api_bg'})
+                    data={'size':'auto','bg_color':blue},
+                    headers={'X-Api-Key': api_bg})
                     if response.status_code == requests.codes.ok:
                        st.image(img, width=300)
                     else:
