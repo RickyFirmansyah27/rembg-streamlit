b534eb7261ea43b089ad4288989f42387b6dff2d
Ricky Firmansyah <110955695+RickyFirmansyah27@users.noreply.github.com>
2022-12-16 18:52:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,7 @@ from matplotlib import pyplot as plt
 import streamlit as st
 import os
 
-def remBG():
-    output = remove(file)
-    plt.imshow(output)
-    plt.show()
 
-def BGblue():
-    image = Image.open(file)
-    output = remove_background(image, color='red')
-    plt.imshow(output)
-    plt.show()
 
 def BGbred():
     old_name = file.name
@@ -33,7 +24,6 @@ if __name__=="__main__":
     if file is not None:
         st.sidebar.write('File Uploaded')
         st.write('Program API REMBG')
-        remBG()
         BGbred()
             
  
